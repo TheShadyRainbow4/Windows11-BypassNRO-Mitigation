bd147a3df869a72c60ab63b0796704c767f0e0f3
EliteSoftware Company <123858247+TheShadyRainbow4@users.noreply.github.com>
2025-10-18 03:48:09 -0400

    Update bypass.cmd
    Used correct URL this time for the xml.. oops

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://github.com/TheShadyRainbow4/Windows11-BypassNRO-Mitigation/blob/main/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/TheShadyRainbow4/Windows11-BypassNRO-Mitigation/refs/heads/main/unattend.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
